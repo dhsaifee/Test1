235c5857614f2d9253c8e05c7d04c300d1cc0495
dhsaifee <dhsaifee@cba.ua.edu>
2020-01-13 15:12:17 -0600

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
